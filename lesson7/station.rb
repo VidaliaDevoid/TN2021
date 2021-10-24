cf3cef5d3dde20640718e1e55da353c90591ed3b
Natalie Korosteleva <90615204+VidaliaDevoid@users.noreply.github.com>
2021-10-24 14:33:04 +0300

    Lesson7 Proc clocks and lambda (#7)

---
+++ lesson7/station.rb
@@ -0,0 +1,63 @@
+require_relative 'instance_counter'
+
+class Station
+  include InstanceCounter
+  attr_accessor :trains
+  attr_reader :name
+
+  @@stations = []
+
+  def self.all
+    @@stations
+  end
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    @@stations << self
+    validate!
+    register_instance
+  end
+
+  def get_trains_by_type(type)
+    trains.select{|t| t.type == type}
+  end
+
+  def add_train(train)
+    trains << train
+  end
+
+  def remove_train(train)
+    trains.delete(train)
+  end
+
+  def trains_list
+    unless trains.empty?
+      puts "On station '#{self.name}':"
+      trains.each.with_index do |train, index|
+        puts "#{index + 1}) #{train.number} - #{train.type} train"
+      end
+    end
+  end
+
+  def each_train
+    trains.each { |t| yield(t) }
+  end
+
+  def list_trains
+    each_train do |t|
+      puts "##{t.number} #{t.type} -- carriage count: #{t.carriages.count}."
+    end
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
+  end
+
+  def validate!
+    raise ArgumentError, 'Empty or nil name station.' if name.nil? || name.empty?
+  end
+end
